28ad4c00f852db8a2b96157095169592549ffa94
YOGISHOBAN <yogishoban1996@gmail.com>
2016-12-26 20:30:19 -0800

    Add files via upload

---
+++ POSITIVE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("given number is positive");
+}
+if(a==0)
+{
+printf("given number is zero");
+}
+if(a<0)
+{
+printf("given number is negative");
+}}
